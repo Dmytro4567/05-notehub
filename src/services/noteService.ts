35606a28787b880cf6fd4f16b6505d0f19e0ed6c
Dmytro <dmitry.dvernikov@gmail.com>
2025-06-05 17:36:41 +0300

    add all components

---
+++ src/services/noteService.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import type {Note} from '../types/note';
+
+const BASE_URL = 'https://notehub-public.goit.study/api/notes';
+const TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+const instance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        Authorization: `Bearer ${TOKEN}`,
+    },
+});
+
+export const fetchNotes = async (
+    page = 1,
+    perPage = 12,
+    search = ''
+): Promise<{ notes: Note[]; totalPages: number }> => {
+    const params: Record<string, string | number> = { page, perPage };
+    if (search.trim()) {
+        params.search = search;
+    }
+
+    const response = await instance.get('', { params });
+    return response.data;
+};
+
+
+export const createNote = async (
+    noteData: Omit<Note, 'id'>
+): Promise<Note> => {
+    const response = await instance.post('', noteData);
+    return response.data;
+};
+
+export const deleteNote = async (id: string): Promise<Note> => {
+    const response = await instance.delete(`/${id}`);
+    return response.data;
+};
